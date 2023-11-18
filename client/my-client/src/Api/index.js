680baa386f7a42b517c0df07e64bd82d635bec35
Rajkumarp02 <rajkumarprjpm@gmail.com>
2023-11-18 18:21:46 +0530

    add render link to client

--- client/my-client/src/Api/index.js
+++ client/my-client/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 //create Api
-const API = axios.create({baseURL:"http://localhost:8000"})
+const API = axios.create({baseURL:"https://expense-zq04.onrender.com"})
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
